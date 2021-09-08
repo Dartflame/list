5d9051dcfc9d0a1654a903c8623ce6f64dc08ffc
Dartflame <flamemaskin@gmail.com>
2021-09-08 19:51:09 +0300

    lkj

--- heroku-create-app.sh
+++
@@ -1,2 +0,0 @@
-heroku apps:create dartflame-list-app
-heroku addons:create heroku-postgresql:hobby-dev --app dartflame-list-app
--- heroku-deploy-app.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-heroku container:release web --app=geek-shopping-list
